db9d17928921e3b68f62761d4e5839829de902a2
leonardolima <leonardo.alfs@gmail.com>
2019-11-18 14:02:06 -0300

    Testing Crank-Nicolson

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,15 @@
 /******************************/
 
 #include "bandwidth_minimization.h"
+#include "crank_nicolson.h"
 
 int main (void)
 {
-    execute_algorithm(20);
+    //execute_algorithm(20);
+
+    int N = 51;
+    float L = 1;
+    float dt = 5.e-4;
+    int nsteps = 620;
+    diffusion_1d(N, L, dt, nsteps);
 }
