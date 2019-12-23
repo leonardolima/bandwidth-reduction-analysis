2501b32ef1caced12f8b64759fd6875a06fc024b
leonardolima <leonardo.alfs@gmail.com>
2019-12-23 14:15:22 -0300

    Fixing solvers plot file

--- src/plot/solvers.py
+++ src/plot/solvers.py
@@ -3,14 +3,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("out.csv", sep=',', names=['N', 'lubksb', 'tridag'])
+df = pd.read_csv("solvers_data.csv", sep=',', names=['N', 'lubksb', 'tridag'])
 
 ax = plt.gca()
 
 df.plot(kind='line', x='N', y='lubksb', ax=ax)
 df.plot(kind='line', x='N', y='tridag', ax=ax)
-
 plt.title('Comparison of lubksb and tridag methods')
+
 plt.xlabel('N')
-plt.ylabel('milliseconds')
+plt.ylabel('microseconds')
 plt.show()
