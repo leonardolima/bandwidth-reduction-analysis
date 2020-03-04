609c6328f105d6a76516132780f695d2543dc9b9
leonardolima <leonardo.alfs@gmail.com>
2020-03-04 01:53:22 -0300

    Adding print functions

--- src/io.cpp
+++ src/io.cpp
@@ -1,9 +1,33 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <queue>
+#include <set>
 #include <Eigen/Dense>
 
-void print_map(const std::map<std::pair<int, int>, int>& m)
+void print_set (const std::set<int>& s)
+{
+    for (auto it = s.begin(); it != s.end(); ++it)
+    {
+        std::cout << ' ' << *it;
+    }
+    std::cout << std::endl;
+}
+
+void print_queue (const std::queue<int>& q)
+{
+    std::queue<int> q_copy = q;
+
+    std::cout << "q = ";
+    while (!q_copy.empty())
+    {
+        std::cout << q_copy.front() << " ";
+        q_copy.pop();
+    }
+    std::cout << std::endl;
+}
+
+void print_map (const std::map<std::pair<int, int>, int>& m)
 {
     for (const auto &it : m)
     {
@@ -13,7 +37,7 @@ void print_map(const std::map<std::pair<int, int>, int>& m)
     }
 }
 
-void matrix_to_csv(const Eigen::MatrixXf& R)
+void matrix_to_csv (const Eigen::MatrixXf& R)
 {
     const Eigen::IOFormat CSVFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n");
 
--- src/io.h
+++ src/io.h
@@ -5,4 +5,8 @@
 
 void matrix_to_csv(const Eigen::MatrixXf&);
 
+void print_queue(const std::queue<int>&);
+
+void print_set (const std::set<int>& s);
+
 #endif
