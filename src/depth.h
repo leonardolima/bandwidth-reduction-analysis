becc65d2e10dc87ee53c9665d020ff8a3ef0045a
leonardolima <leonardo.alfs@gmail.com>
2020-03-04 01:56:45 -0300

    Finishing naive approach using depth as criteria

---
+++ src/depth.h
@@ -0,0 +1,8 @@
+#ifndef DEPTH_H
+#define DEPTH_H
+
+#include <Eigen/Dense>
+
+void apply_depth(const Eigen::MatrixXf&, Eigen::MatrixXf&);
+
+#endif
--- src/peak_mem.cpp
+++ src/peak_mem.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include <vector>
 #include <Eigen/Dense>
+#include "basic.h"
 #include "adapted_cuthill_mckee.h"
+#include "depth.h"
 
 /*******************************************************************************
  * Lists each integer of a particular string.
@@ -50,7 +52,7 @@ int read_N_from_file (const std::string& file_name)
  * n2 o2 n1 (i.e., node 2 has output size o2 and receives n1 as input)
  *
  * @param file_name String containing file's name.
- * @param A Adjacency matrix of the graph (symmetric).
+ * @param A Adjacency matrix of the graph.
  * @param O Ordering constraints (given in the form L_x > [L_yi]).
  ******************************************************************************/
 void read_file (const std::string& file_name, Eigen::MatrixXf& A,
@@ -77,14 +79,24 @@ void read_file (const std::string& file_name, Eigen::MatrixXf& A,
         for (std::vector<int>::size_type j = 1; j < tokens.size(); ++j)
         {
             O[i].push_back(tokens[j]);
-            A(tokens[j], i) = A(tokens[j], tokens[j]); // potentially using A(j, j) before initialisation ?
-            A(i, tokens[j]) = A(tokens[j], tokens[j]);
+            A(i, tokens[j]) = A(tokens[j], tokens[j]); // potentially using A(j, j) before initialisation ?
         }
 
         i++;
     }
 
     file.close();
+}
+
+void apply_symmetry (Eigen::MatrixXf& A)
+{
+    for (int i = 0; i < A.rows(); ++i)
+    {
+        for (int j = 0; j < A.cols(); ++j)
+        {
+            if (A(i,j) != 0) A(j, i) = A(i, j);
+        }
+    }
 }
 
 /*******************************************************************************
@@ -97,8 +109,8 @@ void read_file (const std::string& file_name, Eigen::MatrixXf& A,
  * @param O     Ordering constraints (in the form L_x > [L_yi]).
  * @param new_O Ordering constraints (in the form L_x < [L_yi]).
  ******************************************************************************/
-void convert_vector(const std::vector<std::vector<int>>& O,
-                    std::vector<std::vector<int>>& new_O)
+void convert_vector (const std::vector<std::vector<int>>& O,
+                     std::vector<std::vector<int>>& new_O)
 {
     for (std::vector<std::vector<int>>::size_type i = 0; i < O.size(); ++i)
     {
@@ -109,11 +121,23 @@ void convert_vector(const std::vector<std::vector<int>>& O,
     }
 }
 
-void peak_mem(const std::string& file_name)
+void test_depth (const Eigen::MatrixXf& A, Eigen::MatrixXf& P, Eigen::MatrixXf& R)
+{
+    apply_depth(A, P);
+
+    R = (P*A*P.transpose()); // Resulting matrix
+
+    std::cout << "Applying depth approach: " << std::endl;
+    std::cout << "dim(A) = " << A.rows() << "x" << A.cols() << std::endl;
+    std::cout << "bandwidth(A) = " << matrix_bandwidth(A) << std::endl;
+    std::cout << "bandwidth(R) = " << matrix_bandwidth(R) << std::endl;
+}
+
+void peak_mem (const std::string& file_name)
 {
     int N = read_N_from_file(file_name);
 
-    // Adjacency matrix representing the graph (symmetric)
+    // Adjacency matrix representing the graph
     Eigen::MatrixXf A = Eigen::MatrixXf::Identity(N, N);
 
     // Permutation matrix (in case of Cuthill-McKee algorithm)
@@ -132,5 +156,8 @@ void peak_mem(const std::string& file_name)
 
     convert_vector(O, new_O);
 
-    apply_adapted_cuthill_mckee(A, P, R, new_O);
+    // Remember to call apply_symmetry() first
+    // apply_adapted_cuthill_mckee(A, P, R, new_O);
+
+    test_depth(A, P, R);
 }
