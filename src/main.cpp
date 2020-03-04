be70bb2b67a1c15842caf63159a605566b8c93aa
leonardolima <leonardo.alfs@gmail.com>
2020-03-04 01:54:49 -0300

    Iterating over files in folder data_sets

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+#include <filesystem>
 #include "peak_mem.h"
 
 int main (void)
 {
-    peak_mem("data_sets/example.txt");
+    std::string folder = "data_sets/";
+    for (const auto& file : std::filesystem::directory_iterator(folder))
+    {
+        std::string file_name = file.path().filename().string();
+
+        std::cout << "File: " << file_name << std::endl;
+        peak_mem("data_sets/" + file_name);
+        std::cout << std::endl;
+    }
 }
