8cc45ba8712c3436b69b7e14020a9472a84490d7
leonardolima <leonardo.alfs@gmail.com>
2020-10-04 13:15:01 +0100

    Refactoring Cuthill-McKee implementation

--- src/bandwidth_analysis/cuthill_mckee.cpp
+++ src/bandwidth_analysis/cuthill_mckee.cpp
@@ -18,33 +18,30 @@
  * @param P Permutation matrix.
  * @param col Node's column index on the permutation matrix.
  ******************************************************************************/
-bool node_has_label (const Eigen::MatrixXf& P, unsigned int col)
+bool node_has_label(const Eigen::MatrixXf& P, unsigned int col)
 {
     for (int i = 0; i < P.rows(); ++i) if (P(i,col) != 0) return true;
-
     return false;
 }
 
-int node_label (const Eigen::MatrixXf& P, unsigned int col)
+int node_label(const Eigen::MatrixXf& P, unsigned int col)
 {
     for (int i = 0; i < P.rows(); ++i) if (P(i, col) != 0) return P(i, col);
-
     return -1;
 }
 
 /*******************************************************************************
- * Returns node's index on the adjacency matrix (the one representing the graph)
+ * Returns node's index on the adjacency matrix (the matrix representing the graph)
  * given its label.
  *
  *
  * @param P Permutation matrix.
  * @param label Node's label.
  ******************************************************************************/
-int node_index (const Eigen::MatrixXf& P, int label)
+int node_index_adj_matrix(const Eigen::MatrixXf& P, int label)
 {
     // Index on the adjacency matrix corresponds to column's index
     for(int j = 0; j < P.cols(); ++j) if (P(label, j) != 0) return j;
-
     return -1;
 }
 
@@ -54,29 +51,29 @@ int node_index (const Eigen::MatrixXf& P, int label)
  *
  *
  * @param node_row Node's row on adjacency matrix.
- * @param rows_deg Vector with the degree of each node's rows.
+ * @param nodes_deg Vector with the degree of each node's rows.
  * @param P Permutation matrix.
  ******************************************************************************/
-std::vector<std::pair<int, int>> sort_rows_deg (const Eigen::VectorXf& node_row,
-                                                const std::vector<int>& rows_deg,
+std::vector<std::pair<int, int>> sort_adj_nodes(const Eigen::VectorXf& adj_nodes,
+                                                const std::vector<int>& nodes_deg,
                                                 const Eigen::MatrixXf& P)
 {
-    std::vector<std::pair<int, int>> sorted_rows_deg;
+    std::vector<std::pair<int, int>> sorted_adj_nodes;
 
-    for(int i = 0; i < node_row.size(); ++i)
+    for(int i = 0; i < adj_nodes.size(); ++i)
     {
         // We are not interested if the element is on the diagonal
         // or if the node already has a label
-        if (node_row[i] != 0 && !node_has_label(P, i))
+        if (adj_nodes[i] != 0 && !node_has_label(P, i))
         {
-            std::pair<int, int> pair = std::make_pair(rows_deg[i], i);
-            sorted_rows_deg.push_back(pair);
+            std::pair<int, int> pair = std::make_pair(nodes_deg[i], i);
+            sorted_adj_nodes.push_back(pair);
         }
     }
 
-    std::sort(sorted_rows_deg.begin(), sorted_rows_deg.end());
+    std::sort(sorted_adj_nodes.begin(), sorted_adj_nodes.end());
 
-    return sorted_rows_deg;
+    return sorted_adj_nodes;
 }
 
 /*******************************************************************************
@@ -87,31 +84,31 @@ std::vector<std::pair<int, int>> sort_rows_deg (const Eigen::VectorXf& node_row,
  *
  * @param A Adjacency matrix.
  * @param P Permutation matrix.
- * @param rows_deg Vector with the degree of each node's rows.
+ * @param nodes_deg Vector with the degree of each node's rows.
  ******************************************************************************/
-void nodal_numbering (const Eigen::MatrixXf& A, Eigen::MatrixXf& P, const std::vector<int>& rows_deg)
+void nodal_numbering(const Eigen::MatrixXf& A, Eigen::MatrixXf& P, const std::vector<int>& nodes_deg)
 {
     unsigned int new_label = 1;
 
     // 3. Label unconnected nodes
     for(int j = 0; j < A.cols(); ++j)
     {
-        if (rows_deg[j] == 0) P(new_label++, j) = 1; // Update matrix P accordingly
+        if (nodes_deg[j] == 0) P(new_label++, j) = 1; // Update matrix P accordingly
     }
 
     // 4. For every node we (i) list the adjacent nodes (ii) sort them in ascending order
     // of degree (iii) label them uniquely and accordingly (2, 3, ...)
     for(int j = 0; j < A.cols(); ++j)
     {
-        if (rows_deg[j] > 0)
+        if (nodes_deg[j] > 0)
         {
-            int node_row_index = node_index(P, j);
-            Eigen::VectorXf node_row = A.row(node_row_index);
-            std::vector<std::pair<int, int>> sorted_rows_deg = sort_rows_deg(node_row, rows_deg, P);
+            int node_index = node_index_adj_matrix(P, j);
+            Eigen::VectorXf adj_nodes = A.row(node_index);
+            std::vector<std::pair<int, int>> sorted_adj_nodes = sort_adj_nodes(adj_nodes, nodes_deg, P);
 
-            for (std::vector<std::pair<int, int>>::size_type i = 0; i < sorted_rows_deg.size(); ++i)
+            for (std::vector<std::pair<int, int>>::size_type i = 0; i < sorted_adj_nodes.size(); ++i)
             {
-                P(new_label++, sorted_rows_deg[i].second) = 1; // Update matrix P accordingly
+                P(new_label++, sorted_adj_nodes[i].second) = 1; // Update matrix P accordingly
             }
         }
     }
@@ -123,20 +120,19 @@ void nodal_numbering (const Eigen::MatrixXf& A, Eigen::MatrixXf& P, const std::v
  *
  * @param R Resulting matrix after applying Cuthill-McKee algorithm.
  ******************************************************************************/
-int matrix_deg (const Eigen::MatrixXf& R)
+int matrix_deg(const Eigen::MatrixXf& R)
 {
-    std::vector<int> rows_deg(R.rows(), 0);
+    std::vector<int> nodes_deg(R.rows(), 0);
 
     for (int i = 0; i < R.rows(); ++i)
     {
         for(int j = 0; j < R.cols(); ++j)
         {
-            if (R(i,j) != 0 && i != j) rows_deg[i] += 1;
+            if (R(i,j) != 0 && i != j) nodes_deg[i] += 1;
         }
     }
 
-    auto max_deg = *std::max_element(rows_deg.begin(), rows_deg.end());
-
+    auto max_deg = *std::max_element(nodes_deg.begin(), nodes_deg.end());
     return max_deg;
 }
 
@@ -147,19 +143,19 @@ int matrix_deg (const Eigen::MatrixXf& R)
  *
  * @param A Adjacency matrix.
  ******************************************************************************/
-std::vector<int> compute_rows_deg (const Eigen::MatrixXf& A)
+std::vector<int> compute_nodes_deg(const Eigen::MatrixXf& A)
 {
-    std::vector<int> rows_deg(A.rows(), 0); // A is square
+    std::vector<int> nodes_deg(A.rows(), 0); // A is square
 
     for(int j = 0; j < A.cols(); ++j)
     {
         for (int i = 0; i < A.rows(); ++i)
         {
-            if (A(i,j) != 0 && i != j) rows_deg[i] += 1;
+            if (A(i,j) != 0 && i != j) nodes_deg[i] += 1;
         }
     }
 
-    return rows_deg;
+    return nodes_deg;
 }
 
 /*******************************************************************************
@@ -169,12 +165,12 @@ std::vector<int> compute_rows_deg (const Eigen::MatrixXf& A)
  *
  * @param A Adjacency matrix.
  ******************************************************************************/
-std::vector<int> select_starting_nodes (const Eigen::MatrixXf& A)
+std::vector<int> select_starting_nodes(const Eigen::MatrixXf& A)
 {
     std::vector<int> starting_nodes;
 
     // 1. Compute degree of every row, where deg(i) = \Sum_{j!=i} a_{ij} != 0
-    std::vector<int> rows_deg = compute_rows_deg(A);
+    std::vector<int> nodes_deg = compute_nodes_deg(A);
 
     // 2. Select starting node (usually the one with minimum degree)
     // Permuting rows and columns of matrix A is done using the
@@ -187,14 +183,14 @@ std::vector<int> select_starting_nodes (const Eigen::MatrixXf& A)
 
     // We should not consider rows that only have a single element on
     // the main diagonal when computing starting_nodes
-    for(std::vector<int>::size_type i = 0; i < rows_deg.size(); ++i)
+    for(std::vector<int>::size_type i = 0; i < nodes_deg.size(); ++i)
     {
-        if (rows_deg[i] > 0 && rows_deg[i] < min_deg) min_deg = rows_deg[i];
+        if (nodes_deg[i] > 0 && nodes_deg[i] < min_deg) min_deg = nodes_deg[i];
     }
 
-    for(std::vector<int>::size_type i = 0; i < rows_deg.size(); ++i)
+    for(std::vector<int>::size_type i = 0; i < nodes_deg.size(); ++i)
     {
-        if (rows_deg[i] == min_deg) starting_nodes.push_back(i);
+        if (nodes_deg[i] == min_deg) starting_nodes.push_back(i);
     }
 
     return starting_nodes;
@@ -212,19 +208,19 @@ std::vector<int> select_starting_nodes (const Eigen::MatrixXf& A)
  * @param P Permutation matrix.
  * @param R Lowest degree resulting matrix (R = P*A*P^T).
  ******************************************************************************/
-void compute_matrices (const std::vector<int>& starting_nodes, const Eigen::MatrixXf& A,
+void compute_matrices(const std::vector<int>& starting_nodes, const Eigen::MatrixXf& A,
                       Eigen::MatrixXf& P, Eigen::MatrixXf& R)
 {
     int max_mat_deg = std::numeric_limits<int>::max(); // max_mat_deg = \infty
 
     Eigen::MatrixXf M = Eigen::MatrixXf::Zero(A.rows(), A.cols());
 
-    std::vector<int> rows_deg = compute_rows_deg(A);
+    std::vector<int> nodes_deg = compute_nodes_deg(A);
 
     for(std::vector<int>::size_type i = 0; i < starting_nodes.size(); ++i)
     {
         P(0, starting_nodes[i]) = 1;          // Starting node is labeled as 0
-        nodal_numbering(A, P, rows_deg);      // Execute algorithm on A
+        nodal_numbering(A, P, nodes_deg);     // Execute algorithm on A
         M = (P*A*P.transpose());              // Compute resulting matrix M
         int mat_deg = matrix_deg(M);          // Compute resulting matrix degree
         if (mat_deg < max_mat_deg) R = M;     // Keep matrix M with lowest degree
@@ -243,7 +239,7 @@ void compute_matrices (const std::vector<int>& starting_nodes, const Eigen::Matr
  *
  * @param A Zeroed adjacency matrix.
  ******************************************************************************/
-void generate_binary_random_matrix (Eigen::MatrixXf& A)
+void generate_binary_random_matrix(Eigen::MatrixXf& A)
 {
     int dimension = A.rows();
 
@@ -266,13 +262,13 @@ void generate_binary_random_matrix (Eigen::MatrixXf& A)
  * @param P Permutation matrix.
  * @param R Lowest degree resulting matrix (R = P*A*P^T).
  ******************************************************************************/
-void run_algorithm (const Eigen::MatrixXf& A, Eigen::MatrixXf& P, Eigen::MatrixXf& R)
+void run_algorithm(const Eigen::MatrixXf& A, Eigen::MatrixXf& P, Eigen::MatrixXf& R)
 {
     std::vector<int> starting_nodes = select_starting_nodes(A);
     compute_matrices(starting_nodes, A, P, R);
 }
 
-void run_tests (int dimension)
+void run_tests(int dimension)
 {
     // Eigen::MatrixXf A(10, 10);
     // A << 1, 1, 0, 1, 0, 0, 0, 0, 1, 0,
