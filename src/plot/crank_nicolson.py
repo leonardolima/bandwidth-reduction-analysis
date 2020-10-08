a1e305d5f9f640713f66ab008e7b9ff7a352fb44
leonardolima <leonardo.alfs@gmail.com>
2020-10-08 14:43:26 +0100

    Improving Crank-Nicolson plots

--- src/plot/crank_nicolson.py
+++ src/plot/crank_nicolson.py
@@ -3,49 +3,51 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plot_time_analysis():
-    N = [150, 222, 315, 416, 540, 670]
-    before = [19, 52, 127, 276, 582, 1093]
-    after = [18, 45, 86, 160, 263, 416]
+def plot_runtime():
+    N = [150, 222, 315, 416, 540, 670, 825, 984, 1170, 1358, 1575]
+    before = [15, 42, 106, 231, 491, 919, 1690, 2839, 4750, 7406, 11338]
+    after = [12, 29, 56, 103, 170, 272, 409, 604, 858, 1185, 1602]
 
     df = pd.DataFrame({'N': N, 'before': before, 'after': after})
 
     ax = plt.gca()
 
-    df.plot(kind='line', x='N', y='before', label='Before applying Cuthill-McKee algorithm', ax=ax)
-    df.plot(kind='line', x='N', y='after', label='After applying Cuthill-McKee algorithm', ax=ax)
-    plt.title('Time complexity analysis of the Crank-Nicolson scheme for solving 2D Heat Equation (nsteps=1000)')
+    df.plot(kind='line', x='N', y='before', label='Runtime', ax=ax, color="tab:red", marker='s')
+    df.plot(kind='line', x='N', y='after', label='Runtime after bandwidth reduction', ax=ax, color="tab:blue", marker='s')
+    plt.title('Crank-Nicolson scheme\'s runtime for solving the Heat Equation in 2D (nsteps=1000)')
 
     plt.xticks(N)
-    plt.yticks(before+after)
+    plt.yticks(range(0, 12000, 500))
 
-    plt.xlabel('N')
+    plt.xlabel('Matrix dimension')
     plt.ylabel('seconds')
+    plt.grid(True)
     plt.show()
 
 def plot_bandwidth_reduction():
-    N = [150, 222, 315, 416, 540, 670]
-    before = [26, 55, 63, 91, 100, 153]
-    after = [11, 14, 16, 19, 21, 24]
+    N = [150, 222, 315, 416, 540, 670, 825, 984, 1170, 1358, 1575]
+    before = [26, 55, 63, 91, 100, 153, 165, 209, 222, 299, 315]
+    after = [11, 14, 16, 19, 21, 24, 26, 29, 31, 34, 36]
 
     df = pd.DataFrame({'N': N, 'before': before, 'after': after})
 
     ax = plt.gca()
 
-    df.plot(kind='line', x='N', y='before', label='Before applying Cuthill-McKee algorithm', ax=ax)
-    df.plot(kind='line', x='N', y='after', label='After applying Cuthill-McKee algorithm', ax=ax)
-    plt.title('Bandwidth of Matrices')
+    df.plot(kind='line', x='N', y='before', label='Bandwidth', ax=ax, color="tab:red", marker='s')
+    df.plot(kind='line', x='N', y='after', label='Bandwidth after reduction', ax=ax, color="tab:blue", marker='s')
+    plt.title('Bandwidth reduction of the Cuthill-McKee algorithm')
 
     plt.xticks(N)
-    plt.yticks(before+after)
+    plt.yticks(range(0, 325, 25))
 
-    plt.xlabel('N')
-    plt.ylabel('Bandwidth')
+    plt.xlabel('Matrix dimension')
+    plt.ylabel('Matrix bandwidth')
+    plt.grid(True)
     plt.show()
 
 def main():
-    #plot_time_analysis()
-    plot_bandwidth_reduction()
+    plot_runtime()
+    # plot_bandwidth_reduction()
 
 if __name__ == "__main__":
     main()
