97ce91ba2fc0a623bba3c04d42d00f004c6491c5
leonardolima <leonardo.alfs@gmail.com>
2020-11-01 00:20:22 +0000

    Updating main and fixing minor issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <filesystem>
 #include <algorithm>
-#include "peak_mem.h"
+#include "test.h"
 
 int main (void)
 {
-    std::vector<std::string> exc_files = {"Pipe_to_Table_yxmc.txt", "Legend_Builder_yxmc.txt"};
+    // std::vector<std::string> exc_files = {"Pipe_to_Table_yxmc.txt", "Legend_Builder_yxmc.txt"};
+    // std::string folder = "data_sets/";
+    // for (const auto& file : std::filesystem::directory_iterator(folder))
+    // {
+    //     std::string file_name = file.path().filename().string();
 
-    std::string folder = "data_sets/";
-    for (const auto& file : std::filesystem::directory_iterator(folder))
-    {
-        std::string file_name = file.path().filename().string();
+    //     if (!(std::find(exc_files.begin(), exc_files.end(), file_name) != exc_files.end()))
+    //     {
+    //         std::cout << "File: " << file_name << std::endl;
+    //         test_all("data_sets/" + file_name);
+    //         std::cout << std::endl;
+    //     }
+    // }
 
-        if (!(std::find(exc_files.begin(), exc_files.end(), file_name) != exc_files.end()))
-        {
-            std::cout << "File: " << file_name << std::endl;
-            peak_mem("data_sets/" + file_name);
-            std::cout << std::endl;
-        }
-    }
-
-    // peak_mem("data_sets/Base64_Encoder_yxmc.txt");
+    test_all("data_sets/Imputation_yxmc.txt");
+    // test_all("data_sets/example.txt");
 }
--- src/peak_mem.h
+++ src/peak_mem.h
@@ -1,6 +1,8 @@
 #ifndef PEAK_MEM_H
 #define PEAK_MEM_H
 
+#include <Eigen/Dense>
+
 int peak_mem(const Eigen::MatrixXf&, const std::vector<int>&);
 
 #endif
