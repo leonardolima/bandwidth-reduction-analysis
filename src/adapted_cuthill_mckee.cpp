2c1afaf204cbec955196071445d66009fa23135e
leonardolima <leonardo.alfs@gmail.com>
2020-11-07 01:29:38 +0000

    Fixing adapted Cuthill-McKee

--- src/adapted_cuthill_mckee.cpp
+++ src/adapted_cuthill_mckee.cpp
@@ -1,186 +1,171 @@
 #include <iostream>
 #include <vector>
 #include <deque>
-#include <utility>
-#include <limits>
-#include <chrono>
+#include <algorithm>
 #include <Eigen/Dense>
-#include "bandwidth_analysis/cuthill_mckee.h"
 #include "adapted_cuthill_mckee.h"
+#include "peak_mem.h"
 #include "basic.h"
 #include "io.h"
 
 /*******************************************************************************
- * Check if constraints are satisfied, i.e., in order to label the current
- * node, it is necessary to check if L_x < [L_yi] for every i.
+ * Implementation of a topological sorting algorithm, based on the depth-first
+ * search algorithm. It goes through all nodes of the graph marking them until
+ * it reaches a node that has already been visited. Since the graph is directed,
+ * it can stop at that point.
+ * mark[node] == 0 => unmarked node
+ * mark[node] == 1 => marked node
  *
  *
- * @param node  Node's index.
- * @param label Possible label at this step for this particular node.
- * @param O     Ordering constraints (in the form L_x < [L_yi]).
- * @param P     Permutation matrix.
+ * @param children     List of children for each node
+ * @param sorted_nodes Resulting vector with sorted nodes
  ******************************************************************************/
-bool check_constraints (int node, int label, Eigen::MatrixXf& P,
-                        const std::vector<std::vector<int>>& prec_O,
-                        const std::vector<std::vector<int>>& succ_O)
+void nodal_numbering(const std::vector<std::vector<int>>& children,
+                     std::vector<int>& path,
+                     std::vector<int>& indegree,
+                     std::vector<int>& outdegree,
+                     std::vector<bool>& marked,
+                     int num_nodes,
+                     int starting_node)
 {
-    bool satisfy_ordering = true, have_labelled_parents = true;
+    std::deque<int> d;
+    int seen = 0;
 
-    for (std::vector<int>::size_type j = 0; j < succ_O[node].size(); ++j)
+    // Disconnected nodes
+    for(int i = 0; i < num_nodes; ++i)
     {
-        int adj_node_label = node_label(P, succ_O[node][j]);
-        if (adj_node_label != -1 && adj_node_label < label) satisfy_ordering = false;
-    }
-
-    for (std::vector<int>::size_type j = 0; j < prec_O[node].size(); ++j)
-    {
-        int adj_node_label = node_label(P, prec_O[node][j]);
-        if (adj_node_label == -1) have_labelled_parents = false;
-    }
-
-    return satisfy_ordering && have_labelled_parents;
-}
-
-void apply_label_to_node (Eigen::MatrixXf& P,
-                          const std::vector<std::vector<int>>& prec_O,
-                          const std::vector<std::vector<int>>& succ_O,
-                          std::deque<int>& d, int node, int& new_label,
-                          std::vector<int>& visited_nodes,
-                          std::vector<bool>& node_in_d)
-{
-    if (!node_has_label(P, node))
-    {
-        if (check_constraints(node, new_label, P, prec_O, succ_O))
+        if(indegree[i] == 0 && outdegree[i] == 0 && !marked[i])
         {
-            P(new_label++, node) = 1;
-            visited_nodes[node] = 1;
-        } else {
-            d.push_back(node);
-            node_in_d[node] = true;
+            path.push_back(i);
+            marked[i] = true;
+            seen++;
         }
     }
-}
 
-/*******************************************************************************
- * Adapted version of the original algorithm in order to consider the constraints
- * imposed. When labeling the adjacent nodes, it is necessary to check if their
- * label respect the constraints or not. If so, we label them accordingly.
- * Otherwise we keep a queue with all the remaining nodes that haven't been
- * labeled already, checking again at each step (i.e., when considering
- * other nodes if labeling is possible).
- *
- *
- * @param A             Adjacency matrix.
- * @param P             Permutation matrix.
- * @param rows_deg      Vector with the degree of each node's rows.
- * @param O             Ordering constraints (in the form L_x < [L_yi]).
- * @param initial_nodes List of nodes of each subgraph of the graph.
- ******************************************************************************/
-void adapted_nodal_numbering(const Eigen::MatrixXf& A, Eigen::MatrixXf& P,
-                             const std::vector<int>& nodes_deg,
-                             const std::vector<std::vector<int>>& prec_O,
-                             const std::vector<std::vector<int>>& succ_O,
-                             const std::vector<int>& initial_nodes)
-{
-    int N = A.cols();
-    int new_label = 0;
+    path.push_back(starting_node);
+    marked[starting_node] = true;
+    seen++;
 
-    std::deque<int> d;
-    std::vector<bool> node_in_d(N, false); // Existence of node in d
-    std::vector<int> visited_nodes(N, 0);
-
-    for (std::vector<int>::size_type i = 0; i < initial_nodes.size(); ++i)
+    // Connected nodes
+    while(seen < num_nodes)
     {
-        // Mark initial_nodes[i] as visited
-        visited_nodes[initial_nodes[i]] = 1;
-
-        // Push initial_nodes[i] to d
-        d.push_back(initial_nodes[i]);
-        node_in_d[initial_nodes[i]] = true;
-
-        while (!d.empty())
+        for(int i = 0; i < num_nodes; ++i)
         {
-            int u = d.front();
-            d.pop_front();
-            node_in_d[u] = false;
-
-            apply_label_to_node(P, prec_O, succ_O, d, u, new_label, visited_nodes, node_in_d);
-
-            for (std::deque<int>::size_type i = 0; i < d.size(); ++i)
+            if(indegree[i] == 0 && !marked[i])
             {
-                int n = d.front();
-                d.pop_front();
-                node_in_d[n] = false;
-
-                apply_label_to_node(P, prec_O, succ_O, d, n, new_label, visited_nodes, node_in_d);
-            }
-
-            if (nodes_deg[u] > 0)
-            {
-                Eigen::VectorXf adj_nodes = A.row(u);
-                std::vector<std::pair<int, int>> sorted_adj_nodes = sort_adj_nodes(adj_nodes, nodes_deg, P);
-
-                for (std::vector<std::pair<int, int>>::size_type i = 0; i < sorted_adj_nodes.size(); ++i)
+                for(std::vector<int>::size_type j = 0; j < children[i].size(); ++j)
                 {
-                    int cur_node = sorted_adj_nodes[i].second;
-                    if (visited_nodes[cur_node] == 0 && check_constraints(cur_node, new_label, P, prec_O, succ_O))
-                    {
-                        P(new_label++, cur_node) = 1;
-                        visited_nodes[cur_node] = 1;
-                    }
-
-                    if (node_in_d[cur_node] == false)
-                    {
-                        d.push_back(cur_node);
-                        node_in_d[cur_node] = true;
-                    }
+                    path.push_back(children[i][j]);
+                    indegree[children[i][j]]--;
+                    marked[children[i][j]] = true;
+                    seen++;
                 }
 
-                if (node_in_d[u] == false) visited_nodes[u] = 2;
+                path.push_back(i);
+                marked[i] = true;
             }
+
         }
     }
 }
 
-std::vector<int> select_initial_nodes (const Eigen::MatrixXf& A)
+std::vector<int> select_starting_nodes(const Eigen::MatrixXf& A,
+                                       const std::vector<std::vector<int>>& parents)
 {
-    int N = A.rows();
+    std::vector<int> starting_nodes;
 
-    std::vector<int> initial_nodes;
-    std::vector<std::vector<int>> parents(N);
-
-    parents_from_matrix(A, parents);
-
-    // Find all source nodes
-    for (std::vector<std::vector<int>>::size_type i = 0; i < parents.size(); ++i)
+    for(std::vector<std::vector<int>>::size_type i = 0; i < parents.size(); ++i)
     {
-        if (parents[i].size() == 0) initial_nodes.push_back(i);
+        if(parents[i].size() == 0) starting_nodes.push_back(i);
     }
 
-    return initial_nodes;
+    return starting_nodes;
+}
+
+void sort_children(const Eigen::MatrixXf& A, std::vector<std::vector<int>>& children)
+{
+    std::vector<std::pair<int, int>> pairs;
+
+    std::vector<int> nodes_deg = compute_nodes_deg(A);
+
+    for(std::vector<std::vector<int>>::size_type i = 0; i < children.size(); ++i)
+    {
+        for(std::vector<int>::size_type j = 0; j < children[i].size(); ++j)
+        {
+            std::pair<int, int> pair = std::make_pair(nodes_deg[children[i][j]], children[i][j]);
+            pairs.push_back(pair);
+        }
+
+        std::sort(pairs.begin(), pairs.end());
+        children[i].clear();
+
+        for(std::vector<std::pair<int, int>>::size_type j = pairs.size(); j >= 0; --j)
+        {
+            children[i].push_back(pairs[j].second);
+        }
+
+        pairs.clear();
+    }
 }
 
 /*******************************************************************************
- * Performs Cuthill-McKee algorithm considering ordering constraint.
+ * Performs an adapted topological sorting to the graph
  *
  *
  * @param A     Adjacency matrix.
- * @param A_sym Symmetric adjacency matrix.
  * @param P     Permutation matrix.
- * @param R     Resulting matrix (i.e., P*A*P^T).
- * @param O     Ordering constraints (in the form L_x < [L_yi]).
  ******************************************************************************/
-void apply_adapted_cuthill_mckee(const Eigen::MatrixXf& A, const Eigen::MatrixXf& A_sym,
-                                 Eigen::MatrixXf& P, Eigen::MatrixXf& R,
-                                 const std::vector<std::vector<int>>& prec_O,
-                                 const std::vector<std::vector<int>>& succ_O)
+void apply_adapted_cuthill_mckee(const Eigen::MatrixXf& A, Eigen::MatrixXf& P)
 {
-    // std::cout << A << std::endl;
+    int num_nodes = A.rows(); // Matrix dimension
 
-    std::vector<int> nodes_deg = compute_nodes_deg(A_sym);
+    Eigen::MatrixXf M = Eigen::MatrixXf::Zero(num_nodes, num_nodes);
+    Eigen::MatrixXf R = Eigen::MatrixXf::Zero(num_nodes, num_nodes);
 
-    // Starting node of each subgraph of A
-    std::vector<int> initial_nodes = select_initial_nodes(A);
+    std::vector<std::vector<int>> children(num_nodes), parents(num_nodes), possible_paths;
+    std::vector<int> indegree(num_nodes), outdegree(num_nodes), starting_nodes;
+    std::vector<bool> marked(num_nodes);
+    std::vector<int> path;
 
-    adapted_nodal_numbering(A_sym, P, nodes_deg, prec_O, succ_O, initial_nodes);
+    // int min_bandwidth = std::numeric_limits<int>::max();
+    int min_peak = std::numeric_limits<int>::max();
+
+    children_from_matrix(A, children);
+    parents_from_matrix(A, parents);
+
+    for(std::vector<std::vector<int>>::size_type i = 0; i < parents.size(); ++i)
+    {
+        indegree[i] = parents[i].size();
+        outdegree[i] = children[i].size();
+    }
+
+    sort_children(A, children);
+
+    starting_nodes = select_starting_nodes(A, parents);
+
+    for(std::vector<int>::size_type i = 0; i < starting_nodes.size(); ++i)
+    {
+        nodal_numbering(children, path, indegree, outdegree, marked, num_nodes, starting_nodes[i]);
+        possible_paths.push_back(path);
+
+        int peak = peak_mem(A, path);
+
+        for(std::vector<int>::size_type j = 0; j < path.size(); ++j)
+        {
+            std::cout << path[j] << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "peak = " << peak << std::endl;
+        // label_sorted_nodes(A, P, make_sorted_pairs(possible_paths[i]));
+        // M = (P*A*P.transpose());
+        // if(peak < min_peak)
+        // {
+        //     R = P;
+        //     min_peak = peak;
+        // }
+        // P.setZero();
+    }
+    // std::cout << "min_peak = " << min_peak << std::endl;
+    // P = R;
 }
--- src/adapted_cuthill_mckee.h
+++ src/adapted_cuthill_mckee.h
@@ -3,6 +3,6 @@
 
 #include <Eigen/Dense>
 
-void apply_adapted_cuthill_mckee (const Eigen::MatrixXf&, const Eigen::MatrixXf&, Eigen::MatrixXf&, Eigen::MatrixXf&, const std::vector<std::vector<int>>&, const std::vector<std::vector<int>>&);
+void apply_adapted_cuthill_mckee(const Eigen::MatrixXf&, Eigen::MatrixXf&);
 
 #endif
